e6c1ada4d5ccef842b85e4e016d080565be88853
Sullyvan Ogawa <sullyvanogawa@gmail.com>
2026-05-22 22:25:02 -0300

    Use State - hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import { Container } from './components/Container/Container';
 import { CountDown } from './components/CountDown/CountDown';
 import { Formulario } from './components/Formulario/Formulario';
+import { Footer } from './components/Footer/Footer';
 import { Logo } from './components/Logo/Logo';
 import { Menu } from './components/Menu/Menu';
+// import { Heading } from './components/Heading/Heading';
 
 import './styles/global.css';
 import './styles/themes.css';
 
 export function App() {
+  // let number = 0;
+  // function handleClick() {
+  //   number += 1;
+  // }
+
+  
   return (
     <>
+      {/* <Heading>Número: {number}</Heading> */}
+      {/* <button onClick={handleClick}></button> */}
       <Container>
         <Logo />
       </Container>
@@ -23,7 +33,11 @@ export function App() {
       </Container>
 
       <Container>
-          <Formulario />
+        <Formulario />
+      </Container>
+
+      <Container>
+        <Footer />
       </Container>
     </>
   );
