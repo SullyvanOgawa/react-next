78b77e41358eaa2d2b1225e0ed8be281de7a8ad8
Sullyvan Ogawa <sullyvanogawa@gmail.com>
2026-07-06 14:44:42 -0300

    Criando os Scripts com type script

--- src/models/TaskModel.ts
+++ src/models/TaskModel.ts
@@ -1,3 +1,4 @@
+import type { TaskStateModel } from "./TaskStateModel";
 
 export type TaskModel = {
   id: string;
@@ -6,5 +7,5 @@ export type TaskModel = {
   startDate: number;
   completeDate: Number | null; // quando a tarefa for concluída, o completeDate vai receber a data de conclusão. Caso contrário, ele será nulo.
   interruptedDate: number | null; // quando a tarefa for interrompida, o interruptedDate vai receber a data de interrupção. Caso contrário, ele será nulo.
-  type: 'workTime' | 'shortBreakTime' | 'longBreakTime'; // o type vai indicar se a tarefa é de trabalho, de intervalo curto ou de intervalo longo.
+  type: keyof TaskStateModel['config']; // o type vai indicar se a tarefa é de trabalho, de intervalo curto ou de intervalo longo.
 };
